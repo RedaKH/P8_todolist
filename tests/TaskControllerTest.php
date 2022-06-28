716a289ea1b687c95247cd2eeaa846e69c590bc8
RedaKH <37907191+RedaKH@users.noreply.github.com>
2022-06-28 02:16:04 +0200

    adding-tests

--- tests/TaskControllerTest.php
+++ tests/TaskControllerTest.php
@@ -2,9 +2,99 @@
 
 namespace App\Tests;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Repository\TaskRepository;
+
 
 class TaskControllerTest extends WebTestCase
 {
-   
+
+    public function login(){
+        $client = static::createClient();
+
+        $testUser = static::getContainer()->get(UserRepository::class)->findOneByUser('user');
+        
+        return $client->loginUser($testUser);
+    }
+    public function testMakeTask()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/tasks/create');
+
+        $form = $client->submitForm('Ajouter', [
+            'task[title]' => 'lorem',
+            'task[content]'=>'test'
+        ]);
+
+        $client->submit($form);
+        $crawler = $client->followRedirect();
+        $this->assertSelectorTextContains('div.alert-success', "Superbe ! La tâche a été bien été ajoutée.");
+    }
+
+    public function testEditOwnTask()
+    {
+        $client = $this->login();
+        
+        $crawler = $client->request('GET', '/tasks/6/edit');
+        $this->assertResponseIsSuccessful();
+
+        $form = $crawler->selectButton('Modifier')->form([
+            'task[title]' => 'anothertest',
+            'task[content]' => 'anothertest2'
+        ]);
+
+        $client->submit($form);
+        $crawler = $client->followRedirect();
+        $this->assertSelectorTextContains('div.alert-success', "Superbe ! La tâche a bien été modifiée.");
+    }
+
+    public function testToggleTask()
+    {
+        $client = $this->login();
+        
+        $crawler = $client->request('GET', '/tasks/1/toggle');
+        $this->assertEquals(Response::HTTP_FOUND, $client->getResponse()->getStatusCode());
+        $crawler = $client->followRedirect();
+
+        $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+        $this->assertEquals(1, $crawler->filter('div.alert-success')->count());
+        
+    }
+
+
+
+    public function testListDoneTask()
+    {
+        $client = $this->login();
+
+        $router = $client->getContainer()->get('router');
+
+        $crawler = $client->request('GET', $router->generate('task_list', ['isDone' => 1]));
+
+        $this->assertSelectorExists('.glyphicon.glyphicon-ok');
+    }
+
+    public function testListNoDoneTask()
+    {
+        $client = $this->login();
+
+        $router = $client->getContainer()->get('router');
+
+        $crawler = $client->request('GET', $router->generate('task_list', ['isDone' => 0]));
+
+        $this->assertSelectorNotExists('.glyphicon.glyphicon-ok');
+    }
+
+    public function testDeleteTask()
+    {
+        $client = $this->login();
+        
+        $crawler = $client->request('GET', '/tasks/6/delete');
+        $this->assertEquals(Response::HTTP_FOUND, $client->getResponse()->getStatusCode());
+        $crawler = $client->followRedirect();
+
+        $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+        $this->assertSelectorTextContains('div.alert-success', "Superbe ! La tâche a bien été supprimée.");
+    }
 }
--- tests/UserControllerTest.php
+++ tests/UserControllerTest.php
@@ -2,14 +2,85 @@
 
 namespace App\Tests;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Repository\UserRepository;
 
 class UserControllerTest extends WebTestCase
 {
+   public function loginAdmin(){
+      $client = static::createClient();
+
+      $testUser = static::getContainer()->get(UserRepository::class)->findOneByUser('user');
+      
+      return $client->loginUser($testUser);
+
+   }
+   public function loginUser(){
+      $client = static::createClient();
+
+      $testUser = static::getContainer()->get(UserRepository::class)->findOneByUser('user');
+      
+      return $client->loginUser($testUser);
+
+   }
+
    public function testMakeAccount(){
-    $client = static::createClient();
-    $crawler = $client->request('GET','/user_create');
+      $client = static::createClient();
+      $crawler = $client->request('GET', '/user/create');
+      $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+
+      $form = $crawler->selectButton('Ajouter')->form([
+          'user[username]' => 'test',
+          'user[password][first]' => 'testpassword',
+          'user[password][second]' => 'testpassword',
+          'user[email]' => 'test@test.com',
+          'user[roles]' => 'ROLE_USER'
+      ]);
+
+      $client->submit($form);
+      $crawler = $client->followRedirect();
+      $this->assertSelectorTextContains('div.alert-success', "Superbe ! L'utilisateur a bien été ajouté.");
+
+      $testUser = static::getContainer()->get(UserRepository::class)->findOneBy('test');
+      $this->assertInstanceOf(User::class,$testUser);
 
 
    }
+   public function testSuccesssListUser()
+   {
+       $client = $this->loginAdmin();
+       $crawler = $client->request('GET', '/users');
+
+       $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+       $this->assertSelectorTextContains('h1', 'Liste des utilisateurs');
+   }
+
+   public function testFailListUser()
+   {
+       $client = $this->LoginUser();
+       $crawler = $client->request('GET', '/users');
+
+       $this->assertEquals(Response::HTTP_FORBIDDEN, $client->getResponse()->getStatusCode());
+   }
+
+   public function testUpdateUser()
+   {
+       $client = $this->loginAdmin();
+       $crawler = $client->request('GET', '/users/3/edit');
+       $this->assertResponseIsSuccessful();
+
+       $form = $crawler->selectButton('Modifier')->form([
+           'user[username]' => 'test2',
+           'user[password][first]' => 'test2password',
+           'user[password][second]' => 'test2password',
+           'user[email]' => 'test2@test2.fr'
+       ]);
+
+       $client->submit($form);
+       $this->assertResponseRedirects("/users", Response::HTTP_FOUND);
+       $crawler = $client->followRedirect();
+       $this->assertSelectorTextContains('div.alert-success', "Superbe ! L'utilisateur a bien été modifié");
+   }
+
 }
